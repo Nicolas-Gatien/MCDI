cd72052b3f32301feb3f1ef641efe475e1dad9ff
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-06 15:15:06 -0500

    Download File

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-extern crate reqwest;
-
-use std::io::Read;
+use reqwest::{self, blocking::Response};
+use std::fs::File;
+use std::io::Write;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut res = reqwest::get("http://138.197.135.52/api/get/cur")?;
-    let mut body = String::new();
-    res.read_to_string(&mut body)?;
+    let url: &str = "http://138.197.135.52/api/get/cur";
+    let response: Response = reqwest::blocking::get(url)?;
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
+    if response.status().is_success() {
+        let bytes = response.bytes()?;
+        let filename = "downloaded.zip";
+        let mut file = File::create(&filename)?;
+        file.write_all(&bytes)?;
+
+        println!("File download successful! {}", &filename);
+    } else {
+        println!("Failed to download file. Status: {}", response.status());
+    }
 
     Ok(())
 }
