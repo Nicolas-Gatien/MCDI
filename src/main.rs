47bd7aa8d08fc2087443c6347457531dac8c1d06
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-05 20:41:20 -0500

    Add Basic Get Request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut res = reqwest::get("http://138.197.135.52/api/get/cur")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+    println!("Body:\n{}", body);
+
+    Ok(())
 }
