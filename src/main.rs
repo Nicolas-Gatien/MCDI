bd576ab291d6e16fcbfa314b36c35ea689620eb4
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-06 22:18:20 -0500

    Set Destination to Current World

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 use reqwest::{self, blocking::Response};
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Write;
+use whoami;
+use regex::Regex;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn minecraft_folder() -> String {
+    let path: String = format!("/Users/{}/Library/Application Support/minecraft", whoami::username());
+    return path;
+}
+
+fn get_path_minecraft_world() -> String {
+    let logpath: String = format!("{}/logs/latest.log", minecraft_folder());
+    let contents: String = fs::read_to_string(&logpath).expect("Should have been able to read file");
+    
+    let re = Regex::new(r"ServerLevel\[(?<name>[^\]]+)\]").unwrap();
+    match re.captures_iter(&contents).last() {
+        Some(caps) => caps["name"].to_string(),
+        None => "Nothing".to_string()
+    }
+}
+
+fn fetch_datapack() -> Result<(), Box<dyn std::error::Error>> {
     let url: &str = "http://138.197.135.52/api/get/cur";
     let response: Response = reqwest::blocking::get(url)?;
 
@@ -18,4 +36,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+} 
+
+fn main() {
+    let current_world_path = get_path_minecraft_world();
+    let destination = format!("{}/saves/{}/datapacks", minecraft_folder(), &current_world_path);
+
+    println!("{}", &destination);
 }
