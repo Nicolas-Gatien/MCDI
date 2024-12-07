21427bacae73b9dc0df92825a64cf1021eb3903c
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-07 13:44:33 -0500

    Add CLI install command

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use reqwest::{self, blocking::Response};
 use std::fs;
 use whoami;
 use regex::Regex;
+use std::env;
 
 fn minecraft_folder() -> String {
     let path: String = format!("/Users/{}/Library/Application Support/minecraft", whoami::username());
@@ -19,8 +20,8 @@ fn get_current_world_name() -> String {
     }
 }
 
-fn fetch_datapack() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let url: &str = "http://138.197.135.52/api/get/cur";
+fn fetch_datapack(name: &String) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let url: String = format!("http://138.197.135.52/api/get/{}", name);
     let response: Response = reqwest::blocking::get(url)?;
 
     if response.status().is_success() {
@@ -32,14 +33,26 @@ fn fetch_datapack() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 } 
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+    let datapack_name = &args[2];
+
+    if command != "install" {
+        return;
+    }
+
     let world_name = get_current_world_name();
     let destination = format!("{}/saves/{}/datapacks", minecraft_folder(), &world_name);
 
-    let datapack = fetch_datapack();
+    println!("Fetching Datapack: {}", datapack_name);
+
+    let datapack = fetch_datapack(datapack_name);
 
     if datapack.is_ok() {
         let path = format!("{}/datapack.zip", &destination);
         let bytes = datapack.unwrap();
         fs::write(&path, &bytes).expect("Unable to write file");
+    } else {
+        println!("Couldn't find Datapack: {}", datapack_name)
     }
 }
