b9719323731867910788aed7088f3e985fbd630c
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-07 22:49:09 -0500

    Extract to Functions

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,56 @@ fn fetch_datapack(name: &String) -> Result<Vec<u8>, Box<dyn std::error::Error>> 
     }
 } 
 
+fn unzip_datapack(path: &String, destination: String) {
+    let datapack_file = fs::File::open(&path).unwrap();
+    let mut archive = zip::ZipArchive::new(datapack_file).unwrap();
+    let base_destination: &Path = Path::new(&destination);
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+
+        let outpath = match file.enclosed_name() {
+            Some(path) => base_destination.join(path),
+            None => continue,
+        };
+        
+        {
+            let comment = file.comment();
+            if !comment.is_empty() {
+                println!("File {} comment: {}", i, comment);
+            }
+        }
+        if (*file.name()).ends_with('/'){
+            println!("File {} extracted to \"{}\"", i, outpath.display());
+            fs::create_dir_all(&outpath).unwrap();
+        } else {
+            println!("File {} extracted to \"{}\" ({} bytes)", i, outpath.display(), file.size());
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(&p).unwrap();
+                }
+            }
+            let mut outfile = fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+            
+            if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+            }
+        }
+
+    }
+}
+
+fn delete_archive(path: &String) -> std::io::Result<()> {
+    fs::remove_file(path)?;
+    Ok(())
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
@@ -55,54 +105,12 @@ fn main() {
 
     if datapack.is_ok() {
         let path = format!("{}/{}.zip", &destination, &datapack_name);
-        let base_destination = Path::new(&destination);
 
         let bytes = datapack.unwrap();
         fs::write(&path, &bytes).expect("Unable to write file");
 
-        let datapack_file = fs::File::open(&path).unwrap();
-        let mut archive = zip::ZipArchive::new(datapack_file).unwrap();
-
-        for i in 0..archive.len() {
-            let mut file = archive.by_index(i).unwrap();
-
-            let outpath = match file.enclosed_name() {
-                Some(path) => base_destination.join(path),
-                None => continue,
-            };
-            
-            {
-                let comment = file.comment();
-                if !comment.is_empty() {
-                    println!("File {} comment: {}", i, comment);
-                }
-            }
-            if (*file.name()).ends_with('/'){
-                println!("File {} extracted to \"{}\"", i, outpath.display());
-                fs::create_dir_all(&outpath).unwrap();
-            } else {
-                println!("File {} extracted to \"{}\" ({} bytes)", i, outpath.display(), file.size());
-                if let Some(p) = outpath.parent() {
-                    if !p.exists() {
-                        fs::create_dir_all(&p).unwrap();
-                    }
-                }
-                let mut outfile = fs::File::create(&outpath).unwrap();
-                io::copy(&mut file, &mut outfile).unwrap();
-            }
-
-            #[cfg(unix)]
-            {
-                use std::os::unix::fs::PermissionsExt;
-                
-                if let Some(mode) = file.unix_mode() {
-                    fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
-                }
-            }
-
-        }
-
-        
+        unzip_datapack(&path, destination);
+        delete_archive(&path);
     } else {
         println!("Couldn't find Datapack: {}", datapack_name)
     }
